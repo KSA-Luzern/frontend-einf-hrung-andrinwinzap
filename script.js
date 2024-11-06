dfad75417f8c66eda0b0bdaa6f365bc422a396a1
AndrinWinzap <AndrinWinzap@proton.me>
2024-11-06 09:55:23 +0100

    add input

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
- 
+let state = true;
 
+function go() {
+    text = document.getElementById("input").value;
+
+    let color = "yellow";
+
+    if (text.includes("angry")) {
+        console.log("angry");
+        color = "red";
+    }
+
+    document.getElementById("face").style.backgroundColor = color;
+}
